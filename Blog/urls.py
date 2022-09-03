7719c237dab5b0f3a080239a08684cf1141a327c
mahdiramazani <mahdiramazani1281@gmail.com>
2022-09-03 20:37:43 +0430

    add view delet,add,update and detail Article

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.ArticleView.as_view())
+    path("",views.ArticleView.as_view()),
+    path("detail/<int:pk>",views.ArticleDetail.as_view()),
+    path("add",views.ArticleAddView.as_view()),
+    path("update/<int:pk>",views.ArticleUpdateView.as_view()),
+    path("delete/<int:pk>",views.ArticleDeleteView.as_view())
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -4,12 +4,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from Blog.models import Article
 from Blog.serializers import ArticleSerializer
-
-@api_view(["POST","GET"])
-def tset(requset):
-
-
-    return Response({"messgae":"Hi"})
+from rest_framework import status
+# @api_view(["POST","GET"])
+# def tset(requset):
+#
+#
+#     return Response({"messgae":"Hi"})
 
 
 class ArticleView(APIView):
@@ -21,6 +21,57 @@ class ArticleView(APIView):
 
 
         return Response(data=serializer.data)
+
+
+class ArticleDetail(APIView):
+
+    def post(self,request,pk):
+        query_set=Article.objects.get(id=pk)
+        serializer=ArticleSerializer(instance=query_set)
+
+
+        return Response(data=serializer.data)
+
+
+class ArticleAddView(APIView):
+
+    def post(self,request):
+
+        serializer=ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response({"message":"added"},status=status.HTTP_200_OK)
+
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class ArticleUpdateView(APIView):
+
+    def put(self,request,pk):
+        instance=Article.objects.get(id=pk)
+        serializer=ArticleSerializer(data=request.data,partial=True)
+
+        if serializer.is_valid():
+            serializer.update(instance=instance,validated_data=serializer.validated_data)
+            serializer.save()
+
+            return Response({"message":"Updated"},status=status.HTTP_200_OK)
+
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class ArticleDeleteView(APIView):
+
+    def delete(self,request,pk):
+        instance=Article.objects.get(id=pk)
+        instance.delete()
+
+        return Response({"message":"deleted"},status=status.HTTP_200_OK)
+
+
+
+
+
 
 
 
