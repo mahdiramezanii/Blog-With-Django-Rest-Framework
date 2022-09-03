17a17e3aed17f8f40f1251bb6d8520c72a41fc39
mahdiramazani <mahdiramazani1281@gmail.com>
2022-09-03 19:36:26 +0430

    add models serializer article

--- Blog/serializers.py
+++ Blog/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+from Blog.models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Article
+        fields="__all__"
