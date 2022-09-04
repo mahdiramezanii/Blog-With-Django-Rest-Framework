5cecd1bddfafc2ccd00ee0e5add524dc68fc2357
mahdiramazani <mahdiramazani1281@gmail.com>
2022-09-04 10:58:10 +0430

    add field user to model Article

--- Blog/models.py
+++ Blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Article(models.Model):
-
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="article",null=True,blank=True)
     titel=models.CharField(max_length=100)
     created=models.DateTimeField(auto_now_add=True)
     discription=models.TextField()
