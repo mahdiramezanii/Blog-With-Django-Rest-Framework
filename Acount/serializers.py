b32ee032a8baa2b8882b0e27c20509898c784753
mahdiramazani <mahdiramazani1281@gmail.com>
2022-09-07 18:24:07 +0430

    Displaying a user's articles when displaying posts

--- Acount/serializers.py
+++ Acount/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from Blog.models import Article
+from Blog.serializers import ArticleSerializer
 
 class Userserializer(serializers.ModelSerializer):
+    article=serializers.SerializerMethodField()
     username=serializers.CharField(write_only=True)
     password=serializers.CharField(write_only=True)
     class Meta:
         model = User
         fields = "__all__"
 
+
+    def get_article(self,obj):
+
+        instance=ArticleSerializer(instance=obj.article.all(),many=True)
+        return instance.data
