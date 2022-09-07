804a4939a9f714384da1decb6df794ec5ab8c164
mahdiramazani <mahdiramazani1281@gmail.com>
2022-09-07 19:34:04 +0430

    Viewing relationships of serializers model and adding comments of an article to articles

--- Blog/serializers.py
+++ Blog/serializers.py
@@ -2,16 +2,18 @@ from rest_framework import serializers
 from Blog.models import Article,Comment
 
 
-class ArticleSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model=Article
-        fields="__all__"
-        read_only_fields=["id",]
-
 class CommentSerializer(serializers.ModelSerializer):
     article=serializers.SlugRelatedField(slug_field="discription",read_only=True)
 
     class Meta:
         model=Comment
         fields="__all__"
+
+class ArticleSerializer(serializers.ModelSerializer):
+    comment=serializers.SlugRelatedField(slug_field="text",read_only=True,many=True)
+    class Meta:
+        model=Article
+        fields="__all__"
+        read_only_fields=["id",]
+
+
